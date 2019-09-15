4acea9bac1d80b464d3911c230fb59a8586be30f
Tabook22 <nmtabook@gmail.com>
2019-09-15 21:05:30 +0400

    fixing crispy form

--- peertotur/models.py
+++ peertotur/models.py
@@ -12,20 +12,20 @@ class Peertotur(models.Model):
         ('Grad_Student','Grad_Student'),
     ]
 
-    pname=models.CharField(max_length=200)
-    paddress=models.CharField(max_length=200)
-    pemail=models.CharField(max_length=100)
-    pmajor=models.CharField(max_length=100)
-    pdep=models.CharField(max_length=100)
-    pgpamajor=models.CharField(max_length=10)
-    pgpacum=models.CharField(max_length=100)
-    pexgraduate=models.IntegerField(max_length=4)
+    pname=models.CharField(verbose_name="Name", max_length=200)
+    paddress=models.CharField(verbose_name="Address",max_length=200)
+    pemail=models.CharField(verbose_name="E-mail address", max_length=100)
+    pmajor=models.CharField(verbose_name="Major",max_length=100)
+    pdep=models.CharField(verbose_name="Department",max_length=100)
+    pgpamajor=models.CharField(verbose_name="GPA in major",max_length=10)
+    pgpacum=models.CharField(verbose_name="Cumulative GPA",max_length=100)
+    pexgraduate=models.DateField(verbose_name="Expected date of Graduation (mm/dd/YYYY)", auto_now_add=False, auto_now=False, blank=True)
     reqdate=models.DateTimeField(auto_now_add=True)
-    ptel=models.CharField(max_length=20)
-    pgsm=models.CharField(max_length=20)
-    yearofstudy=models.CharField(max_length=20, choices=yearofstudy_choices)
+    ptel=models.CharField(verbose_name="Tel",max_length=20)
+    pgsm=models.CharField(verbose_name="GSM",max_length=20)
+    yearofstudy=models.CharField(verbose_name="Year of Study",max_length=20, choices=yearofstudy_choices)
     #attchments=models.FileField(upload_to='files/', null=True, verbose_name="")
-    pimg=models.ImageField(upload_to='peertoturs/img/', null=True, blank=True) # null let it be null on the database and the blank let let it be blank on the form, because we have form validation, someime it will not allow blancks
+    pimg=models.ImageField(verbose_name="Image",upload_to='peertoturs/img/', null=True, blank=True) # null let it be null on the database and the blank let let it be blank on the form, because we have form validation, someime it will not allow blancks
     
     def __str__(self):
         return self.pname
