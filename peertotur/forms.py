8b60402933dc0f9f110a720a81a2121b656a6784
Nasser <nmtabook@gmail.com>
2019-09-27 11:19:20 +0400

    some issues with git

--- peertotur/forms.py
+++ peertotur/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.urls import reverse
 from crispy_forms.helper import FormHelper
-from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Submit, Row, Column, Field
 from .models import Peertotur, Peertoturfile, Peertoturexperties, Document
 
